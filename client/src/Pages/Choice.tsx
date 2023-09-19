67858c5710c69bb4f5855f047d711d068a5d393c
Shaul1kr <shaulikr07@gmail.com>
2023-09-19 18:48:35 +0300

    Added navigator

--- client/src/Pages/Choice.tsx
+++ client/src/Pages/Choice.tsx
@@ -1,16 +1,24 @@
 import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
 
 export default function Choice() {
+  const navigate = useNavigate();
   return (
     <Container>
       <PplContainer>
-        <PeopleOutlineIcon sx={{ fontSize: 50, color: "blue" }} />
+        <PeopleOutlineIcon
+          sx={{ fontSize: 50, color: "blue" }}
+          onClick={() => navigate("/customers")}
+        />
         <Paragraph>Customers</Paragraph>
       </PplContainer>
       <CalenderContainer>
-        <CalendarMonthIcon sx={{ fontSize: 50, color: "blue" }} />
+        <CalendarMonthIcon
+          sx={{ fontSize: 50, color: "blue" }}
+          onClick={() => navigate("/appointment")}
+        />
         <Paragraph>Appointments</Paragraph>
       </CalenderContainer>
     </Container>
@@ -18,9 +26,11 @@ export default function Choice() {
 }
 
 const Container = styled.div`
-  margin: auto;
   gap: 5rem;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 `;
 
 const PplContainer = styled.div`
